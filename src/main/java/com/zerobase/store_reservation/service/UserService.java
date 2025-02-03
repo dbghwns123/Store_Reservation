dadb962ba7bde55f5ffaf8af41d3c5c79b113fed
Ho-JunYu <hojoon1011@gmail.com>
2025-02-03 17:21:42 +0900

    UserService 설명(주석) 추가

--- src/main/java/com/zerobase/store_reservation/service/UserService.java
+++ src/main/java/com/zerobase/store_reservation/service/UserService.java
@@ -17,20 +17,25 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    // Partner 인증 토큰
     @Value("${app.partner-token}")
     private String partnerToken; // application.properties 에서 값을 가져옴
 
 
     public void signUp(SignUpRequestDto signUpRequestDto) {
+        // 회원가입을 할 때 이미 해당 ID 가 있다면 예외발생
         Optional<User> existByName = userRepository.findByName(signUpRequestDto.getUsername());
         if (existByName.isPresent()) {
             throw new IllegalStateException("중복된 이름이 있습니다.");
         }
+
         String name = signUpRequestDto.getUsername();
         String password = passwordEncoder.encode(signUpRequestDto.getPassword());
         String email = signUpRequestDto.getEmail();
 
+        // 기본으로는 일반 user
         UserRole role = UserRole.USER;
+        // Partner 이고 함께 넣어준 토큰 값이 일치한다면 Partner role 부여
         if (signUpRequestDto.isPartner()) {
             if (signUpRequestDto.getPartnerToken().equals(partnerToken)) {
                 role = UserRole.PARTNER;
